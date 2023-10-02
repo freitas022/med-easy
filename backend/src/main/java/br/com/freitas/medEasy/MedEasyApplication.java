92966ed0076350961ad66a7500253e7753da3112
Matheus <mf022rj@gmail.com>
2023-10-02 18:13:45 -0300

    Primeiro commit

---
+++ backend/src/main/java/br/com/freitas/medEasy/MedEasyApplication.java
@@ -0,0 +1,13 @@
+package br.com.freitas.medEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedEasyApplication.class, args);
+	}
+
+}
